da6a125ef1c5bc4974f0dc20a96e87ff1e551b91
Khoa280703 <buidangkhoa2807@gmail.com>
2024-09-18 22:54:22 +0700

    fix cart route

--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -104,7 +104,7 @@ const Profile = () => {
               </button>
 
               <Link
-                to="/user-orders"
+                to="/cart"
                 className="bg-pink-600 text-white py-2 px-4 rounded hover:bg-pink-700"
               >
                 My Orders
