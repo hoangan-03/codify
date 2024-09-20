359677552b520c012099f2898fec714ec200fe66
Khoa280703 <buidangkhoa2807@gmail.com>
2024-09-20 11:58:15 +0700

    add falvicon

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import favicons from '@peterek/vite-plugin-favicons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), favicons('../public/code.jpg')],
   server: {
     proxy: {
       "/api/": "http://localhost:5000",
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import favicons from '@peterek/vite-plugin-favicons'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    favicons('public/code.svg')
+  ],
+
 })
